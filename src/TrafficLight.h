7ded296b05473682936d0e82883c6683a0cd9a87
Nickdom1 <48835536+Nickdom1@users.noreply.github.com>
2019-10-14 18:55:46 -0400

    Update to sync up with Udacity workspace

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -19,9 +19,12 @@ template <class T>
 class MessageQueue
 {
 public:
-
+    void send(T &&msg);
+    T receive();
 private:
-    
+    std::deque<T> _queue;
+    std::condition_variable _condition;
+    std::mutex _mutex;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
@@ -29,15 +32,20 @@ private:
 // as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
+enum TrafficLightPhase { 
+    red, 
+    green 
+};
 
-class TrafficLight
+class TrafficLight : public TrafficObject
 {
 public:
-    // constructor / desctructor
-
-    // getters / setters
-
-    // typical behaviour methods
+    TrafficLight();
+    ~TrafficLight() {}
+    void waitForGreen();
+    void simulate();
+    TrafficLightPhase getCurrentPhase();
+    void cycleThroughPhases();
 
 private:
     // typical behaviour methods
@@ -46,8 +54,12 @@ private:
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
 
+    TrafficLightPhase _currentPhase;
+
     std::condition_variable _condition;
     std::mutex _mutex;
+
+    MessageQueue<TrafficLightPhase> _queue;
 };
 
 #endif
